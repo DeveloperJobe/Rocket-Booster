fe6295854f0f866ddab247165c2bbee59c60b448
Jobe <jthom383@montgomerycollege.edu>
2018-09-21 22:35:30 -0400

    Rocket launches, end of day 1.
    Need to add rotation and lock rotation.

--- Assets/Scripts/RocketScript.cs
+++ Assets/Scripts/RocketScript.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class RocketScript : MonoBehaviour {
 
+    Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        rb = GetComponent<Rigidbody>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        ProcessInput();		
 	}
+
+    void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+
+        }
+    }
 }
