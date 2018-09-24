3029345f1877a8332b36fa93fbbd98f26e7b61ba
Jobe <jthom383@montgomerycollege.edu>
2018-09-23 22:57:19 -0400

    End of Day 2
    Many changes!
    -Rocket works fully.
    -2 new levels, 1 being sandbox.
    -Added sounds
    -TODO:  Win/Lose conditions

--- Assets/Scripts/RocketScript.cs
+++ Assets/Scripts/RocketScript.cs
@@ -2,34 +2,74 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
+
 public class RocketScript : MonoBehaviour {
 
     Rigidbody rb;
+    bool rocketPlay;
+    AudioSource rocketAudio;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         rb = GetComponent<Rigidbody>();
+        rocketAudio = GetComponent<AudioSource>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();		
+        Thrust();
+        Rotate();	
 	}
 
-    void ProcessInput()
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Landing":
+                print("Landing..."); //todo remove
+                break;
+            case "Fuel":
+                print("Fueling up..."); //todo remove
+                break;
+            default:
+                print("Dead."); //todo kill player
+                break;
+        }
+    }
+
+    void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rb.AddRelativeForce(Vector3.up);
+            if (!rocketAudio.isPlaying)
+            {
+                rocketAudio.Play();
+            }
+            float forceThisFrame = mainThrust * Time.deltaTime;
+            rb.AddRelativeForce(Vector3.up * forceThisFrame, ForceMode.Force);
         }
+        else if (rocketAudio.isPlaying)
+        {
+            rocketAudio.Stop();
+        }
+    }
+
+    void Rotate()
+    {
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+        rb.freezeRotation = true; // Manual control
 
         if (Input.GetKey(KeyCode.A))
         {
-
+            transform.Rotate(Vector3.forward * rotationThisFrame, Space.Self);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-
+            transform.Rotate(-Vector3.forward * rotationThisFrame, Space.Self);
         }
+        rb.freezeRotation = false; // Resume physics
     }
 }
